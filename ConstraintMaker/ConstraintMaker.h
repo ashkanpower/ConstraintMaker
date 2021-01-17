d71ac9f36b9d05832c1dd2e3772dd44c78fab614
ashkan <ghodrat>
2021-01-17 12:14:36 +0330

    init

---
+++ ConstraintMaker/ConstraintMaker.h
@@ -0,0 +1,18 @@
+//
+//  ConstraintMaker.h
+//  ConstraintMaker
+//
+//  Created by ashkan ghodrat on 10/28/1399 AP.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ConstraintMaker.
+FOUNDATION_EXPORT double ConstraintMakerVersionNumber;
+
+//! Project version string for ConstraintMaker.
+FOUNDATION_EXPORT const unsigned char ConstraintMakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConstraintMaker/PublicHeader.h>
+
+
---
+++ ConstraintMakerExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConstraintMakerExample
+//
+//  Created by ashkan ghodrat on 10/28/1399 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
